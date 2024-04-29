12203ce618bea571c1b4b024cbb51bd9215c3877
StephenAndrewShepstone <111452460+StephenAndrewShepstone@users.noreply.github.com>
2024-04-29 03:46:34 +0100

    Adds notes for test plan
    The test plan includes properties of graphs that may want to be considered for test coverage.

--- Kata.Graphs.Tests/GraphDependencyGeneratorTests.cs
+++ Kata.Graphs.Tests/GraphDependencyGeneratorTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,29 @@ using System.Threading.Tasks;
 
 namespace Kata.Graphs.Tests
 {
-    internal class GraphDependencyGenerator
+    [TestClass]
+    public class GraphDependencyGenerator
     {
+        /*
+            Independent variables
+            VL - Number of vertex layers - 0, one, many
+            VD Number of dependencies on each vertex - 0, one, many
+            DL Number of dependent layers - 0, one, many
+            DBL Number of dependencies between layers - 0, one, many
+	
+            CD Number of cyclic dependencies 0, one, many
+	            - one with gap in line
+	            - one no gap in line
+	            - many complete
+	            - many non-complete
+	
+            GDL - Gap between dependent layers
+
+            Need at least many VL before VD can be considered.
+            Need at least many VL before DL can be considered.
+            DBL dependent on DL and VD.
+            CD dependent on DBL and DL. 
+            GDL dependent on VL and DL.
+         */
     }
 }
