4d9b23548bf2adc11809e10bd8f6700fd5b9cdea
StephenAndrewShepstone <111452460+StephenAndrewShepstone@users.noreply.github.com>
2024-04-30 20:08:19 +0100

    Creates graph and parsing code instantiation code
    This class will contain a graph representation and code for flattening the chain to get all dependencies.
    
    It's not clear yet how the class split will be: there may be a separate class for parsing from the list of strings and for the graph representation, and possibly even for the flattening of the graph itself.

---
+++ Kata.Graphs/Graph.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Graphs
+{
+    public class Graph
+    {
+        private Dictionary<char, LinkedList<char>> _verticesAndDirectNeighbours;
+
+        public Graph(List<string> vertexTransitiveDependencies)
+        {
+            //Validate();
+            _verticesAndDirectNeighbours = GetVerticesAndDirectNeighboursFromStringList(vertexTransitiveDependencies);
+        }
+
+
+        private Dictionary<char, LinkedList<char>> GetVerticesAndDirectNeighboursFromStringList(List<string> vertexTransitiveDependencies)
+        {
+            var dictionary = new Dictionary<char, LinkedList<char>>();
+            try
+            {
+                foreach (string vertexNeighbourString in vertexTransitiveDependencies)
+                {
+                    var neighbours = new LinkedList<char>();
+                    for (int i = 1; i < vertexNeighbourString.Length; i += 2)
+                    {
+                        neighbours.AddLast(vertexNeighbourString[i]);
+                    }
+                    dictionary.Add(vertexNeighbourString[0], neighbours);
+                }
+                return dictionary;
+            }
+            catch (Exception)
+            {
+                throw new InvalidOperationException("The list of strings could not be parsed as a valid graph representation.");
+            }
+        }
+    }
+}
