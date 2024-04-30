f0fb51e36e5de596739d75145c85b58d16b8c81e
StephenAndrewShepstone <111452460+StephenAndrewShepstone@users.noreply.github.com>
2024-04-30 21:30:58 +0100

    Adds first cut of DFS algorithm
    The algorithm flattens the graph representation stored in the classes internal dictionary. It merges visited node dependencies into the current vertex's linked list chain. The merge method has not currently been made but will be added in a future commit.

--- Kata.Graphs/Graph.cs
+++ Kata.Graphs/Graph.cs
@@ -17,6 +17,55 @@ namespace Kata.Graphs
             _verticesAndDirectNeighbours = GetVerticesAndDirectNeighboursFromStringList(vertexTransitiveDependencies);
         }
 
+        public void FlattenGraphDependencies()
+        {
+            var dependencyStack = new Stack<LinkedListNode<char>>();
+            var visited = new HashSet<char>();
+            foreach(KeyValuePair<char, LinkedList<char>> keyValuePair in _verticesAndDirectNeighbours)
+            {
+                if (visited.Contains(keyValuePair.Key))
+                {
+                    continue;
+                }
+                var currentNode = keyValuePair.Value.First;
+                //if (currentNode is not null)
+                //{
+                //    dependencyStack.Push(currentNode);
+                //    visited.Add(currentNode.Value);
+                //}
+                while (dependencyStack.Count > 0 || currentNode is not null)
+                {
+                    if (currentNode is null)
+                    {
+                        currentNode = dependencyStack.Pop();
+                    }
+                    else
+                    {
+                        if (visited.Contains(currentNode.Value))
+                        {
+                            //var currentNode = dependencyStack.Peek();
+                            //Todo: for better performance add another hashset to ensure unneccessary 
+                            //merges aren't taking place if a node has been merged at a deeper level.
+                            LinkedList<char> innerList = _verticesAndDirectNeighbours[currentNode.Value];
+                            LinkedList<char> outerList = keyValuePair.Value;
+                            innerList.Merge<char>(outerList);
+                            currentNode = currentNode.Next;
+                        }
+                        else
+                        {
+                            dependencyStack.Push(currentNode);
+                            visited.Add(currentNode.Value);
+                            currentNode = _verticesAndDirectNeighbours[currentNode.Value].First;
+                        }
+                    }
+                }
+            }
+        }
+
+        //private void PerformDepthFirstSearchWithFlatten()
+        //{
+
+        //}
 
         private Dictionary<char, LinkedList<char>> GetVerticesAndDirectNeighboursFromStringList(List<string> vertexTransitiveDependencies)
         {
---
+++ Kata.Graphs/LinkedListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Graphs
+{
+    internal static class LinkedListExtensions<T> where T: IEquatable<T>, IComparable<T>
+    {
+        internal static void Merge(this LinkedList<T> innerlinkedList, LinkedList<T> outerLinkedList)
+        {
+            
+        }
+    }
+}
