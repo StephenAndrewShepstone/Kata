7d5520e594444c4d65a5ad6da88a5fb2989c10cc
StephenAndrewShepstone <111452460+StephenAndrewShepstone@users.noreply.github.com>
2024-05-04 16:53:19 +0100

    Adds sorted singly linked list implementation
    This is used to represent the graph. It includes the merge algorithm previously added to the linked list extensions.
    
    The linked list extensions is probably no longer needed, so it has had its code commented out and will be removed in future.

--- Kata.Graphs/LinkedListExtensions.cs
+++ Kata.Graphs/LinkedListExtensions.cs
@@ -7,46 +7,46 @@ using System.Threading.Tasks;
 
 namespace Kata.Graphs
 {
-    internal static class LinkedListExtensions
-    {
-        internal static void MergeSortedLinkedLists<T>(this LinkedList<T> innerlinkedList, LinkedList<T> outerLinkedList) where T: IEquatable<T>, IComparable<T>
-        {
-            if (innerlinkedList is null) throw new ArgumentNullException(nameof(innerlinkedList));
-            if (outerLinkedList is null) throw new ArgumentNullException(nameof(outerLinkedList));
+    //internal static class LinkedListExtensions
+    //{
+    //    internal static void MergeSortedLinkedLists<T>(this LinkedList<T> innerlinkedList, LinkedList<T> outerLinkedList) where T: IEquatable<T>, IComparable<T>
+    //    {
+    //        if (innerlinkedList is null) throw new ArgumentNullException(nameof(innerlinkedList));
+    //        if (outerLinkedList is null) throw new ArgumentNullException(nameof(outerLinkedList));
 
-            if (innerlinkedList.First is null)
-            {
-                innerlinkedList.First = outerLinkedList.First;
-                return;
-            }
-            if (outerLinkedList.First is null)
-            {
-                return;
-            }
+    //        if (innerlinkedList.First is null)
+    //        {
+    //            innerlinkedList.First = outerLinkedList.First;
+    //            return;
+    //        }
+    //        if (outerLinkedList.First is null)
+    //        {
+    //            return;
+    //        }
 
-            LinkedListNode<T> innerNode = innerlinkedList.First;
-            LinkedListNode<T> outerNode = outerLinkedList.First;
-            LinkedListNode<T> swapNode;
-            while (innerNode.Next is not null && outerNode is not null)
-            {
-                if (innerNode.Next.Value.CompareTo(outerNode.Value) == 1)
-                {
-                    swapNode = innerNode.Next;
-                    innerNode.Next = outerNode;
-                    outerNode = outerNode.Next;
-                    innerNode = innerNode.Next;
-                    innerNode.Next = swapNode;
-                }
-                else if (innerNode.Next.Value.CompareTo(outerNode.Value) == 0) 
-                {
-                    outerNode = outerNode.Next;
-                }
-                else
-                {
-                    innerNode = innerNode.Next; 
-                }
-            }
-            if (innerNode.Next is null) innerNode.Next = outerNode;
-        }
-    }
+    //        LinkedListNode<T> innerNode = innerlinkedList.First;
+    //        LinkedListNode<T> outerNode = outerLinkedList.First;
+    //        LinkedListNode<T> swapNode;
+    //        while (innerNode.Next is not null && outerNode is not null)
+    //        {
+    //            if (innerNode.Next.Value.CompareTo(outerNode.Value) == 1)
+    //            {
+    //                swapNode = innerNode.Next;
+    //                innerNode.Next = outerNode;
+    //                outerNode = outerNode.Next;
+    //                innerNode = innerNode.Next;
+    //                innerNode.Next = swapNode;
+    //            }
+    //            else if (innerNode.Next.Value.CompareTo(outerNode.Value) == 0) 
+    //            {
+    //                outerNode = outerNode.Next;
+    //            }
+    //            else
+    //            {
+    //                innerNode = innerNode.Next; 
+    //            }
+    //        }
+    //        if (innerNode.Next is null) innerNode.Next = outerNode;
+    //    }
+    //}
 }
---
+++ Kata.Graphs/SortedSinglyLinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Graphs
+{
+    internal class SortedSinglyLinkedList<T> where T : IEquatable<T>, IComparable<T>
+    {
+        internal SinglyLinkedListNode<T>? Head { get; set; }
+
+        internal SortedSinglyLinkedList(SinglyLinkedListNode<T> headOfSortedLinkedList)
+        {
+            Head = headOfSortedLinkedList;
+        }
+
+        internal void MergeSortedLinkedList(SortedSinglyLinkedList<T> outerLinkedList)
+        {
+            if (outerLinkedList is null) throw new ArgumentNullException(nameof(outerLinkedList));
+
+            if (Head is null)
+            {
+                Head = outerLinkedList.Head;
+                return;
+            }
+            if (outerLinkedList.Head is null)
+            {
+                return;
+            }
+
+            SinglyLinkedListNode<T> innerNode = Head;
+            SinglyLinkedListNode<T> outerNode = outerLinkedList.Head;
+            SinglyLinkedListNode<T> swapNode;
+            while (innerNode.Next is not null && outerNode is not null)
+            {
+                if (innerNode.Next.Value.CompareTo(outerNode.Value) == 1)
+                {
+                    swapNode = innerNode.Next;
+                    innerNode.Next = outerNode;
+                    outerNode = outerNode.Next;
+                    innerNode = innerNode.Next;
+                    innerNode.Next = swapNode;
+                }
+                else if (innerNode.Next.Value.CompareTo(outerNode.Value) == 0)
+                {
+                    outerNode = outerNode.Next;
+                }
+                else
+                {
+                    innerNode = innerNode.Next;
+                }
+            }
+            if (innerNode.Next is null) innerNode.Next = outerNode;
+        }
+    }
+}
