3417f959925eeb670fdb83ca71614b0a0aff07c3
StephenAndrewShepstone <111452460+StephenAndrewShepstone@users.noreply.github.com>
2024-05-04 15:05:29 +0100

    Adds tests for singly linked list and merging
    These are needed for the internal testing of linked lists. The tests don't need to cover all cases for linked list merging - just the case for when they are sorted. This reduction in scope should mean that the linked list implementation and required algorithms can be written for the required scenario (graph representation and _sorted_ linked list merging)

---
+++ Kata.Graphs.Tests/LinkedListExtensionsTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Graphs.Tests
+{
+    internal class LinkedListExtensionsTests
+    {
+        private List<int>[] _arrayOfTestLists = new List<int>[]
+        {
+            new List<int> { }, //0
+            new List<int> { 1 }, //1
+            new List<int> { 1, 2 }, //2
+            new List<int> { 3, 4 }, //3
+            new List<int> { 1, 3 }, //4
+            new List<int> { 2, 4 }, //5
+            new List<int> { 1, 3, 5}, //6
+            new List<int> { 0, 2, 4, 6 }, //7
+            new List<int> { 1, 4, 7 }, //8
+
+            new List<int> { 1, 2, 3, 4 }, //9
+            new List<int> { 1, 2, 3, 4, 5 }, //10
+            new List<int> { 0, 1, 2, 3, 4, 5, 6 }, //11
+            new List<int> { 1, 3, 4, 5, 7 }, //12
+        };
+
+        [TestMethod]
+        [DataRow(0, 0, 0, DisplayName = "Two empty lists.")]
+        [DataRow(0, 1, 1, DisplayName = "In place list is empty, added list is not.")]
+        [DataRow(1, 0, 1, DisplayName = "In place list has entry, added list is empty.")]
+
+        //Duplicates
+        [DataRow(1, 2, 2, DisplayName = "One duplicate in smaller inner list.")]
+        [DataRow(2, 1, 2, DisplayName = "One duplicate in smaller outer list.")]
+        [DataRow(2, 2, 2, DisplayName = "Two duplicates.")]
+
+        //No gap with overlaps
+        [DataRow(2, 3, 9, DisplayName = "No gap left overlap of inner list")]
+        [DataRow(3, 2, 9, DisplayName = "No gap right overlap of inner list")]
+
+        //Single gap with overlaps
+        [DataRow(4, 5, 9, DisplayName = "Single gap left overlap of inner list")]
+        [DataRow(5, 4, 9, DisplayName = "Single gap right overlap of inner list")]
+
+        //Multiple gaps with multiple overlaps
+        [DataRow(5, 6, 10, DisplayName = "Two gaps in outer list")]
+        [DataRow(6, 5, 10, DisplayName = "Two gaps in inner list")]
+
+        //Two sided overlap with gaps.
+        [DataRow(6, 7, 11, DisplayName = "Two sided overlap of inner list with gaps")]
+        [DataRow(7, 6, 11, DisplayName = "Two sided overlap of outer list with gaps")]
+
+        //Multiple inter overlaps
+        [DataRow(6, 8, 12, DisplayName = "Inner list multiple value gap.")]
+        [DataRow(8, 6, 12, DisplayName = "Outer list multiple value gap.")]
+        public void MergedSortedLinkedListTests(int testList1Number, int testList2Number, int expectedListAfterMerge)
+        {
+            SinglyLinkedList<int> linkedList1 = GetTestLinkedList(testList1Number);
+            SinglyLinkedList<int> linkedList2 = GetTestLinkedList(testList2Number);
+            List<int> expectedMergedList = _arrayOfTestLists[expectedListAfterMerge];
+
+            linkedList1.MergeLinkedList(linkedList2);
+
+            int entry = 0;
+            SinglyLinkedListNode<int> listNode = linkedList1.Head;
+            while (listNode != null)
+            {
+                Assert.AreEqual(expectedMergedList[entry], listNode.Value, "The lists aren't value equal");
+                listNode = listNode.Next;
+                entry++;
+            }
+            Assert.AreEqual(_arrayOfTestLists[expectedListAfterMerge].Count, entry, "The merged list is not of the expected length.");
+        }
+    }
+}
