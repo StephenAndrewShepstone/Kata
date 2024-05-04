03b8c00d1d6ae69303e71f2d45ec1f693653dd4a
StephenAndrewShepstone <111452460+StephenAndrewShepstone@users.noreply.github.com>
2024-05-04 16:48:23 +0100

    Adds singly linked list node implementation
    This will be used by a sorted linked list class that will be used to represent the graph.

---
+++ Kata.Graphs/SinglyLinkedListNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Graphs
+{
+    internal class SinglyLinkedListNode<T> where T : IEquatable<T>, IComparable<T>
+    {
+        internal SinglyLinkedListNode<T>? Next { get; set; }
+
+        internal T Value { get; set; }
+
+        internal SinglyLinkedListNode()
+        {
+            Value = Value;
+        }
+
+        internal SinglyLinkedListNode(T Value)
+        {
+            Value = Value;
+        }
+    }
+}
