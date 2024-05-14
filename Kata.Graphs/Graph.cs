fca6a96d5f3229dce1e51735e9a98b9e78007f1c
StephenAndrewShepstone <111452460+StephenAndrewShepstone@users.noreply.github.com>
2024-05-14 22:56:46 +0100

    Adds a graph bug fixes and adds GetDependencyListOfAllVertexDependencies
    This includes a new flattened hashset for determining when a vertex has had all dependents flattened into it's linked list chain. Also adds a few bug fixes related to merging and skipping visiting certain dependents.
    
    Moreover, the GetDependencyListOfAllVertexDependencies method gets a list of the flattened chains, which will be the output from the graph flattening process.

--- Kata.Graphs/Graph.cs
+++ Kata.Graphs/Graph.cs
@@ -22,16 +22,18 @@ namespace Kata.Graphs
         {
             var dependencyStack = new Stack<SinglyLinkedListNode<char>>();
             var visited = new HashSet<char>();
-            foreach(KeyValuePair<char, SortedSinglyLinkedList<char>> keyValuePair in _verticesAndDirectNeighbours)
+            var flattened = new HashSet<char>();
+            foreach (KeyValuePair<char, SortedSinglyLinkedList<char>> keyValuePair in _verticesAndDirectNeighbours)
             {
-                if (visited.Contains(keyValuePair.Key))
+                if (flattened.Contains(keyValuePair.Key))
                 {
                     continue;
                 }
                 var currentNode = keyValuePair.Value.Head;
+                
                 //if (currentNode is not null)
                 //{
-                //    dependencyStack.Push(currentNode);
+                //    //dependencyStack.Push(currentNode);
                 //    visited.Add(currentNode.Value);
                 //}
                 while (dependencyStack.Count > 0 || currentNode is not null)
@@ -39,6 +41,7 @@ namespace Kata.Graphs
                     if (currentNode is null)
                     {
                         currentNode = dependencyStack.Pop();
+                        flattened.Add(currentNode.Value);
                     }
                     else
                     {
@@ -47,20 +50,64 @@ namespace Kata.Graphs
                             //var currentNode = dependencyStack.Peek();
                             //Todo: for better performance add another hashset to ensure unneccessary 
                             //merges aren't taking place if a node has been merged at a deeper level.
-                            SortedSinglyLinkedList<char> innerList = _verticesAndDirectNeighbours[currentNode.Value];
-                            SortedSinglyLinkedList<char> outerList = keyValuePair.Value;
+                            //SortedSinglyLinkedList<char> innerList = _verticesAndDirectNeighbours[currentNode.Value];
+                            SortedSinglyLinkedList<char> outerList;
+                            if (_verticesAndDirectNeighbours.TryGetValue(currentNode.Value, out SortedSinglyLinkedList<char> dependencies))
+                            {
+                                outerList = dependencies;
+                            }
+                            else
+                            {
+                                outerList = new SortedSinglyLinkedList<char>();
+                            }
+
+                            SortedSinglyLinkedList<char> innerList = dependencyStack.Any() ? _verticesAndDirectNeighbours[dependencyStack.Peek().Value] : keyValuePair.Value;
                             innerList.MergeSortedLinkedList(outerList);
                             currentNode = currentNode.Next;
                         }
                         else
                         {
-                            dependencyStack.Push(currentNode);
                             visited.Add(currentNode.Value);
-                            currentNode = _verticesAndDirectNeighbours[currentNode.Value].Head;
+                            //currentNode = _verticesAndDirectNeighbours[currentNode.Value].Head;
+                            //Problem is potentially here. On cyclic dependency of A
+                            if (!flattened.Contains(currentNode.Value))
+                            {
+                                dependencyStack.Push(currentNode);
+                                if (_verticesAndDirectNeighbours.TryGetValue(currentNode.Value, out SortedSinglyLinkedList<char> dependencies))
+                                {
+                                    currentNode = dependencies != null ? dependencies.Head : null;
+                                }
+                                else
+                                {
+                                    flattened.Add(dependencyStack.Pop().Value);
+                                    currentNode = currentNode.Next;
+                                }
+                            }
                         }
                     }
                 }
+                flattened.Add(keyValuePair.Key);
             }
+        }
+
+        public List<string> GetDependencyListOfAllVertexDependencies()
+        {
+            List<string> vertexDependencies = new List<string>();
+            StringBuilder stringBuilder = new StringBuilder(); 
+            foreach (char vertex in _verticesAndDirectNeighbours.Keys)
+            {
+                stringBuilder.Append(vertex);
+                SinglyLinkedListNode<char> node = _verticesAndDirectNeighbours[vertex].Head;
+                while (node is not null)
+                {
+                    stringBuilder.Append(' ');
+                    stringBuilder.Append(node.Value);
+                    node = node.Next;   
+                }
+                vertexDependencies.Add(stringBuilder.ToString());
+                stringBuilder.Clear();
+            }
+            return vertexDependencies;
         }
 
         //private void PerformDepthFirstSearchWithFlatten()
@@ -97,17 +144,20 @@ namespace Kata.Graphs
             if (sortedVertexDependentString.Length < 1) throw new InvalidOperationException("The string cannot be empty.");
             if (sortedVertexDependentString.Length < 4)
             {
-                var head = new SinglyLinkedListNode<char>(sortedVertexDependentString[2]);
-                var sortedLinkedList = new SortedSinglyLinkedList<char>(head);
-                sortedLinkedList.Head = head;
+                var sortedLinkedList = new SortedSinglyLinkedList<char>();
+                if (sortedVertexDependentString.Length > 1)
+                {
+                    var head = new SinglyLinkedListNode<char>(sortedVertexDependentString[2]);
+                    sortedLinkedList.Head = head;
+                }
                 return new KeyValuePair<char, SortedSinglyLinkedList<char>>(sortedVertexDependentString[0], sortedLinkedList);
             }
 
-            var sortedVertexDependentHead = new SinglyLinkedListNode<char>(sortedVertexDependentString[0]);
+            var sortedVertexDependentHead = new SinglyLinkedListNode<char>(sortedVertexDependentString[2]);
             var currentNode = sortedVertexDependentHead;
-            for (int i = 0; i < sortedVertexDependentString.Length; i++)
+            for (int i = 4; i < sortedVertexDependentString.Length; i++)
             {
-                if (i % 2 == 0)
+                if (i % 2 == 1)
                 {
                     if (sortedVertexDependentString[i] != ' ') throw new InvalidOperationException("The string is not a valid representation of a vertex and it's dependencies.");
                 }
