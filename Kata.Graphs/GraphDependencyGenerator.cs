5dfa3526679c80925fda84ebd13089c7b52840a6
StephenAndrewShepstone <111452460+StephenAndrewShepstone@users.noreply.github.com>
2024-05-14 23:14:19 +0100

    Adds static method for flattening graph representation
    The code simply make use of the graphs own flattening method. This encapsulates state-change code in a static method, allowing the input-output scenarios to be considered / tested.

---
+++ Kata.Graphs/GraphDependencyGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Graphs
+{
+    public static class GraphDependencyGenerator
+    {
+        public static List<string> GetAllDependenciesOfVertices(List<string> vertexTransitiveDependencies)
+        {
+            var graph = new Graph(vertexTransitiveDependencies);
+            graph.FlattenGraphDependencies();
+            return graph.GetDependencyListOfAllVertexDependencies();
+        }
+    }
+}
